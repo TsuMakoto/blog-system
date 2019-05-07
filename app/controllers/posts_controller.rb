72d4645f85ad5fb4f8da792d74bae555cc34fd26
TsuMakoto <tsukamoto@fusic.co.jp>
2019-05-07 19:16:53 +0900

    記事の保存まで処理を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,33 @@
 class PostsController < ApplicationController
   def new
+    @user = User.find_by(id: params[:id])
+    @post = Post.new
+    @categorys = Category.all
+  end
+
+  def create
+    @post = Post.new(
+      user_id: params[:id],
+      content: params[:content],
+      title: params[:title],
+      mst_status_id: 1,
+      category_id: params[:category],
+      post_time: Time.zone.today
+    )
+
+    if @post.save
+      redirect_to("/posts/#{@post.user_id}/show")
+    else
+      render("/posts/#{@post.user_id}/new")
+    end
   end
 
   def show
   end
 
   def all
+  end
+
+  def update
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   # 新規投稿画面
-  get 'posts/new' => 'posts#edit'
+  get 'posts/:id/new' => 'posts#new'
+  # 投稿編集画面
+  get 'posts/:id/edit' => 'post#edit'
+  # 投稿一覧表示
+  get 'posts/:id/show' => 'posts#show'
+  post 'posts/:id/create' => 'posts#create'
+  # 記事更新
+  post 'pos投稿ts/:id/update' => 'posts#update'
 
   # topページ表示
   get '/' => 'home#top'
