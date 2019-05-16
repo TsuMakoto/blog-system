b80dea5eb0893ef28232e5d8cad6ac149a868566
TsuMakoto <tsukamoto@fusic.co.jp>
2019-05-16 11:33:42 +0900

    DB処理内容をview層から移行

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,14 @@ class UsersController < ApplicationController
   # ユーザのホームを表示
   def show
     @user = User.find_by(user_id: params[:id])
+    @limited_model_posts = limited_model(@user.posts)
+    @limited_model_comments = limited_model(@user.comments)
+  end
+
+  private
+
+  # 取得数を制限
+  def limited_model(model)
+    model.order(created_at: :desc).limit(5)
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,2 @@
 module UsersHelper
-  def limited_model(model)
-    model.order(created_at: :desc).limit(5)
-  end
 end
