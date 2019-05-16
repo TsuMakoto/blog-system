6788c51366b347bc37828381225b51896e9a98cd
TsuMakoto <tsukamoto@fusic.co.jp>
2019-05-16 18:06:46 +0900

    issues#23 jsの処理をjsファイルへ移行

---
+++ app/assets/javascripts/posts.js
@@ -0,0 +1,17 @@
+// コメント編集ボタンを押した時のボタンアクション
+function editComment(comment_id) { // eslint-disable-line
+  var comment_area = $('#edit-comment-area' + comment_id);
+  comment_area.removeClass('hidden');
+
+  var comment_display = $('#comment-content' + comment_id);
+  comment_display.addClass('hidden');
+}
+
+// コメント編集キャンセルボタンを押した時のボタンアクション
+function closeComment(comment_id) { // eslint-disable-line
+  var comment_area = $('#edit-comment-area' + comment_id);
+  comment_area.addClass('hidden');
+
+  var comment_display = $('#comment-content' + comment_id);
+  comment_display.removeClass('hidden');
+}
